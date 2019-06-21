d36a9c9f2d3869b1bbff694595d1e94b3d294ee6
Mpa3b3k <45596135+Mpa3b3k@users.noreply.github.com>
2019-06-21 19:53:43 +0300

    Add files via upload

---
+++ jsDiplom.js
@@ -0,0 +1,31 @@
+"use strict";
+
+
+class Profile {
+	
+	constructor(name, real, pass) {
+    this.name = name;
+    this.real = real;
+    this.pass = pass;
+  }
+
+  adding() {
+
+  }
+
+  auth() {
+
+  }
+
+  pay() {
+
+  }
+
+  conv() {
+
+  }
+
+  trans() {
+
+	}
+}
