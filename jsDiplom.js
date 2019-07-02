47edda85b40be89d4b22d353b89490ed52b10b2a
Mpa3b3k <45596135+Mpa3b3k@users.noreply.github.com>
2019-07-02 14:28:46 +0300

    Add files via upload

--- jsDiplom.js
+++ jsDiplom.js
@@ -39,8 +39,22 @@ class Profile {
 
 function course() {
 	
-}
+} // не очень понял как реализовать функцию
 
 function main() {
 
+	const me = new Profile;
+	const you = new Profile;
+
+	me.adding();
+
+	me.auth();
+
+	me.pay();
+
+	me.conv();
+
+	you.adding();
+
+	me.trans(you);
 }
